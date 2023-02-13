7d76843f7a693b68aea625e5764823d315ddb045
Paulo Capelo <paulodcsc@gmail.com>
2023-02-13 00:35:34 -0300

    mouseDown position finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { Slider } from "./components/Slider";
 import { Footer } from "./components/Footer";
 
 function App() {
+  const handleOnDown = async (e: MouseEvent) => {
+    const track = document.getElementById("image-slide")!;
+    track.dataset.mouseDownAt = e.clientX.toString();
+    console.log(track.dataset.mouseDownAt);
+  };
+
+  window.onmousedown = (e) => handleOnDown(e);
 
   return (
     <>
       <Header />
       <Slider />
-      <Footer/>
+      <Footer />
     </>
   );
 }
